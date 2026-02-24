8a9ca95393ca7e606298ed343e3507816e2f3594
vg3668-art <vg3668@srmist.edu.in>
2026-02-24 15:07:32 +0530

    UC1-Printing OOPS to Console

--- OOPSBannerApp.java
+++ OOPSBannerApp.java
@@ -1,20 +1,9 @@
 /**
- * @author [Your Name]
- * @version 1.0
+ * @author [VIGNESH]
+ * @version 1.0 UC1-Print OOPS to Console
  */
 public class OOPSBannerApp {
-
     public static void main(String[] args) {
-        // Now we just call the method instead of writing all the print lines here
-        displayOopsBanner();
-    }
-
-    // This is the new function (method) we created to hold the banner logic
-    public static void displayOopsBanner() {
-        System.out.println(" ****   ****  ****  **** ");
-        System.out.println("*    * *    * *   * * ");
-        System.out.println("*    * *    * ****  **** ");
-        System.out.println("*    * *    * *        *");
-        System.out.println(" ****   ****  *     **** ");
+        System.out.println("OOPS");
     }
 }
